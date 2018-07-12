adba29cb7fc3bd9f2297b2840fedabd4e4cb2b02
Miya Stauss <miyastauss1@gmail.com>
2018-07-12 16:05:09 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,25 @@
-#write out your code here
-
 def least_coins(cents)
-
-#Code your answer here!
-
+  coin_types = {
+    : quarters => 0
+    : dimes => 0
+    : nickles => 0
+    : pennies => 0
+  }
 end
+
+quarters = cents/.25
+  if amount > 0.25
+    puts ""
+    
+    
+dimes = cents/.10
+  if amount
+    
+
+nickles = cents/.05
+  if
+  
+pennies = cents/.01
+
+
+while 
